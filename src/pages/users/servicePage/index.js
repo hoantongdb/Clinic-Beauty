01f4415e7f726be92cc731f757e2a94641a3bdb7
hoantong <hoan26501@gmail.com>
2023-06-24 22:24:12 +0700

    service

--- src/pages/users/servicePage/index.js
+++ src/pages/users/servicePage/index.js
@@ -6,7 +6,9 @@ import ourservices2 from './img/our-service-2.png';
 import ourservices3 from './img/our-service-3.png';
 import ourservices4 from './img/our-service-4.png';
 // import services3 from './Image/main-services-3.png';
-// import blog1 from './Image/blog-1.png';
+import beauty from '../img/servi-1.png';
+import beauty2 from '../img/servi-2.png';
+import beauty3 from '../img/servi-3.png';
 // import blog2 from './Image/blog-2.png';
 // import blog3 from './Image/blog-3.png';
 const ServicePage = () => {
@@ -41,8 +43,86 @@ const ServicePage = () => {
                     </div>
 
                 </div>
-               
+
             </div>
+            <div className="section-beauty">
+                <div className="section-beauty_left">
+                    <img src={beauty} alt="" />
+                </div>
+                <div className="section-beauty_right">
+                    <div className="section-beauty_right_1">
+                        Beauty Consultation
+                    </div>
+                    <div className="section-beauty_right_2">
+                        We services beauty
+                        consultation
+                    </div>
+                    <div className="section-beauty_right_3">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elit, quam suscipit purus donec amet. Egestas volutpat facilisi eu libero.
+                    </div>
+                    <div className="section-beauty_right_4">
+
+
+                        <div title="Ấn vào đây để xem thêm" className="a"> Make an Appointment</div>
+                        <div className="b"><svg width="16" height="17" viewBox="0 0 16 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M8.00938 9.0644L3.75938 13.58C3.46563 13.8921 2.99063 13.8921 2.7 13.58L1.99375 12.8296C1.7 12.5175 1.7 12.0128 1.99375 11.7041L5.00625 8.50327L1.99375 5.30249C1.7 4.99038 1.7 4.48569 1.99375 4.1769L2.69687 3.41987C2.99062 3.10776 3.46562 3.10776 3.75625 3.41987L8.00625 7.9355C8.30312 8.24761 8.30312 8.75229 8.00938 9.0644ZM14.0094 7.9355L9.75937 3.41987C9.46562 3.10776 8.99062 3.10776 8.7 3.41987L7.99375 4.17026C7.7 4.48237 7.7 4.98706 7.99375 5.29585L11.0063 8.49663L7.99375 11.6974C7.7 12.0095 7.7 12.5142 7.99375 12.823L8.7 13.5734C8.99375 13.8855 9.46875 13.8855 9.75937 13.5734L14.0094 9.05776C14.3031 8.75229 14.3031 8.24761 14.0094 7.9355Z" fill="#FF64AE" />
+                        </svg></div>
+                    </div>
+                </div>
+            </div>
+            <div className="section-beauty">
+                
+                <div className="section-beauty_right">
+                    <div className="section-beauty_right_1">
+                        Beauty Consultation
+                    </div>
+                    <div className="section-beauty_right_2">
+                        We services beauty
+                        consultation
+                    </div>
+                    <div className="section-beauty_right_3">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elit, quam suscipit purus donec amet. Egestas volutpat facilisi eu libero.
+                    </div>
+                    <div className="section-beauty_right_4">
+
+
+                        <div title="Ấn vào đây để xem thêm" className="a"> Make an Appointment</div>
+                        <div className="b"><svg width="16" height="17" viewBox="0 0 16 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M8.00938 9.0644L3.75938 13.58C3.46563 13.8921 2.99063 13.8921 2.7 13.58L1.99375 12.8296C1.7 12.5175 1.7 12.0128 1.99375 11.7041L5.00625 8.50327L1.99375 5.30249C1.7 4.99038 1.7 4.48569 1.99375 4.1769L2.69687 3.41987C2.99062 3.10776 3.46562 3.10776 3.75625 3.41987L8.00625 7.9355C8.30312 8.24761 8.30312 8.75229 8.00938 9.0644ZM14.0094 7.9355L9.75937 3.41987C9.46562 3.10776 8.99062 3.10776 8.7 3.41987L7.99375 4.17026C7.7 4.48237 7.7 4.98706 7.99375 5.29585L11.0063 8.49663L7.99375 11.6974C7.7 12.0095 7.7 12.5142 7.99375 12.823L8.7 13.5734C8.99375 13.8855 9.46875 13.8855 9.75937 13.5734L14.0094 9.05776C14.3031 8.75229 14.3031 8.24761 14.0094 7.9355Z" fill="#FF64AE" />
+                        </svg></div>
+                    </div>
+                </div>
+                <div className="section-beauty_left">
+                    <img src={beauty2} alt="" />
+                </div>
+            </div>
+
+            <div className="section-beauty">
+                <div className="section-beauty_left">
+                    <img src={beauty3} alt="" />
+                </div>
+                <div className="section-beauty_right">
+                    <div className="section-beauty_right_1">
+                        Beauty Consultation
+                    </div>
+                    <div className="section-beauty_right_2">
+                        We services beauty
+                        consultation
+                    </div>
+                    <div className="section-beauty_right_3">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elit, quam suscipit purus donec amet. Egestas volutpat facilisi eu libero.
+                    </div>
+                    <div className="section-beauty_right_4">
+
+
+                        <div title="Ấn vào đây để xem thêm" className="a"> Make an Appointment</div>
+                        <div className="b"><svg width="16" height="17" viewBox="0 0 16 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M8.00938 9.0644L3.75938 13.58C3.46563 13.8921 2.99063 13.8921 2.7 13.58L1.99375 12.8296C1.7 12.5175 1.7 12.0128 1.99375 11.7041L5.00625 8.50327L1.99375 5.30249C1.7 4.99038 1.7 4.48569 1.99375 4.1769L2.69687 3.41987C2.99062 3.10776 3.46562 3.10776 3.75625 3.41987L8.00625 7.9355C8.30312 8.24761 8.30312 8.75229 8.00938 9.0644ZM14.0094 7.9355L9.75937 3.41987C9.46562 3.10776 8.99062 3.10776 8.7 3.41987L7.99375 4.17026C7.7 4.48237 7.7 4.98706 7.99375 5.29585L11.0063 8.49663L7.99375 11.6974C7.7 12.0095 7.7 12.5142 7.99375 12.823L8.7 13.5734C8.99375 13.8855 9.46875 13.8855 9.75937 13.5734L14.0094 9.05776C14.3031 8.75229 14.3031 8.24761 14.0094 7.9355Z" fill="#FF64AE" />
+                        </svg></div>
+                    </div>
+                </div>
+            </div>
+
         </div>
     );
 };
--- src/pages/users/theme/footer/index.js
+++ src/pages/users/theme/footer/index.js
@@ -46,11 +46,22 @@ const Footer = () => {
                 </div>
                 <div class="right-footer mid-footer">
                     <div class="title-page"> Information</div>
-                    <div class="item-page"><img src={img5} alt="ins" />Terms & Conditions</div>
-                    <div class="item-page"><img src={img5} alt="ins" /> Condivacy Policy</div>
-                    <div class="item-page"><img src={img5} alt="ins" /> Blog</div>
-                    <div class="item-page"><img src={img5} alt="ins" /> Contact</div>
-                    <button class="arrow-top"><img src={img5} alt="ins" /></button>
+                    <div class="item-page"><svg width="13" height="14" viewBox="0 0 13 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M4.0625 10.1088V3.57523C4.0625 3.12282 4.60949 2.89626 4.92939 3.21616L8.1962 6.48296C8.3945 6.68127 8.3945 7.00281 8.1962 7.20111L4.92939 10.4679C4.60949 10.7878 4.0625 10.5613 4.0625 10.1088Z" fill="#D7DBFF" />
+                    </svg>Terms & Conditions</div>
+                    <div class="item-page"><svg width="13" height="14" viewBox="0 0 13 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M4.0625 10.1088V3.57523C4.0625 3.12282 4.60949 2.89626 4.92939 3.21616L8.1962 6.48296C8.3945 6.68127 8.3945 7.00281 8.1962 7.20111L4.92939 10.4679C4.60949 10.7878 4.0625 10.5613 4.0625 10.1088Z" fill="#D7DBFF" />
+                    </svg> Condivacy Policy</div>
+                    <div class="item-page"><svg width="13" height="14" viewBox="0 0 13 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M4.0625 10.1088V3.57523C4.0625 3.12282 4.60949 2.89626 4.92939 3.21616L8.1962 6.48296C8.3945 6.68127 8.3945 7.00281 8.1962 7.20111L4.92939 10.4679C4.60949 10.7878 4.0625 10.5613 4.0625 10.1088Z" fill="#D7DBFF" />
+                    </svg> Blog</div>
+                    <div class="item-page"><svg width="13" height="14" viewBox="0 0 13 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M4.0625 10.1088V3.57523C4.0625 3.12282 4.60949 2.89626 4.92939 3.21616L8.1962 6.48296C8.3945 6.68127 8.3945 7.00281 8.1962 7.20111L4.92939 10.4679C4.60949 10.7878 4.0625 10.5613 4.0625 10.1088Z" fill="#D7DBFF" />
+                    </svg> Contact</div>
+                    <button class="arrow-top"><svg width="18" height="19" viewBox="0 0 18 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M2.35205 10.5198L1.57158 9.73936C1.24111 9.40889 1.24111 8.87451 1.57158 8.54756L8.40244 1.71318C8.73291 1.38271 9.26729 1.38271 9.59424 1.71318L16.4251 8.54404C16.7556 8.87451 16.7556 9.40889 16.4251 9.73584L15.6446 10.5163C15.3106 10.8503 14.7657 10.8433 14.4388 10.5022L10.4063 6.26943V16.3733C10.4063 16.8409 10.0302 17.2171 9.5626 17.2171H8.4376C7.97002 17.2171 7.59385 16.8409 7.59385 16.3733V6.26943L3.55791 10.5058C3.23096 10.8503 2.68604 10.8573 2.35205 10.5198Z" fill="white" />
+                    </svg>
+                    </button>
                 </div>
             </div>
             <div class="bottom-footer">
