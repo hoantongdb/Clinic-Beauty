aa8217370923f78564b52c2659625062072fc18f
hoantong <hoan26501@gmail.com>
2023-06-28 08:23:16 +0700

    update homepage

--- src/pages/users/homePage/index.js
+++ src/pages/users/homePage/index.js
@@ -11,6 +11,7 @@ import team3 from '../img/team-3.png';
 import FB from '../img/icon-FB.svg';
 import Tiwter from '../img/icon-Twitter.svg';
 import inta from '../img/icon-Intagram.svg';
+import us from './Image/home-aboutus.png';
 const HomePage = () => {
     return (
         <div>
@@ -26,20 +27,20 @@ const HomePage = () => {
                     </div>
                 </div>
                 <div class="background-right">
-                    <img src={ami}alt=""/>
+                    <img src={ami} alt="" />
                 </div>
 
             </div>
             <div className="main-services">
                 <div className="main-services_1">
-                Main Services
+                    Main Services
                 </div>
                 <div className="main-services_2">
-                Learn services to focus
+                    Learn services to focus
                     on your beauty
                 </div>
                 <div className="main-services_3">
-                Porta rhoncus orci condimentum vitae lobortis eu dignissim non massa. Non parturient amet, feugiat
+                    Porta rhoncus orci condimentum vitae lobortis eu dignissim non massa. Non parturient amet, feugiat
                     tellus sagittis, scelerisque eget nulla turpis.
                 </div>
                 <div className="main-blog_bottom">
@@ -84,7 +85,41 @@ const HomePage = () => {
                     </div>
                 </div>
             </div>
+            <div className="section-aboutus">
+                <div className="section-aboutus_left">
+                    <div className="section-aboutus_left1">
+                        About Us
+                    </div>
+                    <div className="section-aboutus_left2">
+                        We are the best beauty clinic
+                    </div>
+                    <div className="section-aboutus_left3">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elit, quam suscipit purus donec amet. Egestas volutpat facilisi eu libero. Nunc, ipsum ornare mauris sit quam quis enim. Varius tellus in suspendisse placerat.
 
+                        Id dui erat sed quam tellus in purus. Pellentesque congue fringilla cras tellus enim.
+                    </div>
+                    <div className="section-aboutus_left4">
+                        <div className="section-aboutus_left41">
+                            <button className="btnus">Learn More</button>
+                        </div>
+                        <div className="section-aboutus_left42">
+                            <svg width="49" height="50" viewBox="0 0 49 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <circle cx="24.5" cy="24.9382" r="24.5" fill="#091156" />
+                                <path d="M32.1167 23.6145C33.45 24.3843 33.45 26.3088 32.1167 27.0786L22.8042 32.4551C21.4708 33.2249 19.8042 32.2627 19.8042 30.7231L19.8042 19.9699C19.8042 18.4303 21.4708 17.4681 22.8042 18.2379L32.1167 23.6145Z" fill="white" />
+                            </svg>
+
+                        </div>
+                        <div className="section-aboutus_left43">
+                        Watch Video
+
+                        </div>
+                        
+                    </div>
+                </div>
+                <div className="section-aboutus_right">
+                <img src={us} alt="" />
+                </div>
+            </div>
             <div className="section-teams">
                 <div className="section-teams_1">
                     Professional Teams
@@ -95,6 +130,7 @@ const HomePage = () => {
                 <div className="section-teams_3">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam.
                 </div>
+                <div className="k"></div>
                 <div className="section-teams_bottom">
                     <div className="section-teams_bottom_box">
                         <div className="section-teams_bottom_box_1">
@@ -163,7 +199,7 @@ const HomePage = () => {
             </div>
             <div class="contact">
                 <div class="contact-left">
-                    <img src={contactimg} alt=""/>
+                    <img src={contactimg} alt="" />
                 </div>
                 <div class="contact-right">
                     <div class="con-title">
@@ -173,25 +209,25 @@ const HomePage = () => {
                         <span>Lorem ipsum dolor sit amet nulla turapis tellus.</span>
 
                     </div>
-                
-                <div class="con-form">
-                    <form >
-                        <div class="name">
-                            <div class="first">
-                                <input type="text" id="fname" class="fname" placeholder="First name" /><br />
+
+                    <div class="con-form">
+                        <form >
+                            <div class="name">
+                                <div class="first">
+                                    <input type="text" id="fname" class="fname" placeholder="First name" /><br />
+                                </div>
+                                <div class="last">
+                                    <input type="text" id="fname" class="fname" placeholder="Last name" /><br />
+                                </div>
                             </div>
-                            <div class="last">
-                                <input type="text" id="fname" class="fname" placeholder="Last name" /><br />
+                            <input type="text" class="fname esy" placeholder="Email address" /><br />
+                            <input type="text" class="fname esy" placeholder="Subject message" /><br />
+                            <input type="text" class="fname esy esyhi" placeholder="Your inquiry here" /><br />
+                            <div class="btn3">
+                                <button>Send Message</button>
                             </div>
-                        </div>
-                        <input type="text" class="fname esy" placeholder="Email address" /><br />
-                        <input type="text" class="fname esy" placeholder="Subject message" /><br />
-                        <input type="text" class="fname esy esyhi" placeholder="Your inquiry here" /><br />
-                        <div class="btn3">
-                            <button>Send Message</button>
-                        </div>
-                    </form>
-                </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
